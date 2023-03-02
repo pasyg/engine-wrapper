82feb815d5905fa5c42dbdbfc31cee9db307958a
pasyg <pgekle@gmail.com>
2023-03-02 17:40:46 +0100

    print result

--- src/gen1/battle_rby.hpp
+++ src/gen1/battle_rby.hpp
@@ -57,6 +57,7 @@ namespace RBY
             pkmn_gen1_battle poke_battle;
             std::copy(battle_arr.begin(), battle_arr.end(), battle_.bytes);
         }
+
         void init() { pkmn_psrng_init(&random, seed); }
         // TODO
         pkmn_choice choose(
@@ -76,18 +77,22 @@ namespace RBY
             // choose an option here is to just use the PSRNG to pick one at random
             return options[(uint64_t)pkmn_psrng_next(random) * n / 0x100000000];
         }
+
         pkmn_choice choose_p1()
         {
             return choose(&battle_, &random, PKMN_PLAYER_P1, pkmn_result_p1(result), options.begin()); 
         }
+
         pkmn_choice choose_p2()
         {
             return choose(&battle_, &random, PKMN_PLAYER_P2, pkmn_result_p2(result), options.begin()); 
         }
+
         pkmn_result play_turn()
         {
             return pkmn_gen1_battle_update(&battle_, c1, c2, buf.begin(), size); 
         }
+
         pkmn_result play_battle()
         {
             init();
@@ -149,6 +154,37 @@ namespace RBY
         turn = turn & state.bytes[368];
 
         return turn;
+    }
+
+    static void print_result(pkmn_gen1_battle& battle, pkmn_result result)
+    {
+        // The battle is written in native endianness so we need to do a bit-hack to
+        // figure out the system's endianess before we can read the 16-bit turn data
+        volatile uint32_t endian = 0x01234567;
+        uint16_t turns = (*((uint8_t *)(&endian))) == 0x67
+            ? battle.bytes[368] | battle.bytes[369] << 8
+            : battle.bytes[368] << 8 | battle.bytes[369];
+
+        //The result is from the perspective of P1
+        switch (pkmn_result_type(result)) {
+            case PKMN_RESULT_WIN: {
+                printf("Battle won by Player A after %d turns\n", turns);
+                break;
+            }
+            case PKMN_RESULT_LOSE: {
+                printf("Battle won by Player B after %d turns\n", turns);
+                break;
+            }
+            case PKMN_RESULT_TIE: {
+                printf("Battle ended in a tie after %d turns\n", turns);
+                break;
+            }
+            case PKMN_RESULT_ERROR: {
+                printf("Battle encountered an error after %d turns\n", turns);
+                break;
+            }
+            default: assert(false);
+        }
     }
 } // namespace RBY
 } // namespace engine
